b0586a742010bd650770867d96f921db83353954
KCsongor76 <kristocsongor0916@gmail.com>
2023-10-11 12:20:51 +0300

    lab4/F2

--- Lab4/F2/testStudent.php
+++ Lab4/F2/testStudent.php
@@ -1,1 +1,10 @@
 <?php
+
+namespace F2;
+
+include "Student.php";
+$student1 = new Student(1, "Elso", "Elso");
+$student2 = new Student(2, "Masodik", "Masodik");
+
+$clone1 = clone $student1;
+$clone2 = clone $student2;
